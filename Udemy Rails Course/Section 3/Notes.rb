1dcdc987e52eb5404b2f2fb0acb4ced1884f8d8e
Laura Aubin <laura.aubin@shopify.com>
2017-09-08 10:25:00 -0400

    Section 3: Intro to Ruby on Rails

---
+++ Udemy Rails Course/Section 3/Notes.rb
@@ -0,0 +1,28 @@
+# -------------------------------------------
+# SECTION 3
+# Intro to Ruby on Rails
+# -------------------------------------------
+
+# HELPERS: Logic for views
+# PUBLIC folder: contains 404, 422, & 500 html files
+
+#FLOW:
+# Request from browser
+# Received at router
+# Request routed to controller
+# Controller either renders a view template, or goes to model
+    # Model communicates with database
+    # Model sends back data to Controller
+    # Controller renders view
+
+get 'wecome/home', to: 'welcome#home'
+# Browser route 'welcome/home' directed to the welcome controller,
+# home action.
+
+class WelcomeController < ApplicationController
+# In file 'welcome_controller.rb', inherits from subclass, 'ApplicationController'
+
+def home
+# Controller action method
+
+# Template
