d8652ad427d172806e3513d27ba8d64a214af176
Laura Aubin <laura.aubin@shopify.com>
2017-09-08 11:13:30 -0400

    Routing, version control, and production

--- Udemy Rails Course/Section 3/Notes.rb
+++ Udemy Rails Course/Section 3/Notes.rb
@@ -15,6 +15,11 @@
     # Model sends back data to Controller
     # Controller renders view
 
+
+# -------------------------------------------
+# TEST APP
+# -------------------------------------------
+
 get 'welcome/home', to: 'welcome#home'
 # Browser route 'welcome/home' directed to the welcome controller,
 # home action.
@@ -41,3 +46,44 @@ root route:
     get 'about', to: 'welcome#about'
     # <%= link_to 'Home', root_path %>
     # <%= link_to 'About', about_path %>
+
+# Create model with a scaffold
+rails generate scaffold Article title:string description:text
+  # Migration file for table
+  # Article model
+  # Test
+  # Routes
+rake db:migrate
+
+# Controller successful save notice (not a flash)
+format.html { redirect_to @object, notice: 'Notice text.' }
+
+
+# -------------------------------------------
+# VERSION CONTROL
+# -------------------------------------------
+
+git init # initialize new repo
+
+Submodules # https://git-scm.com/book/en/v2/Git-Tools-Submodules
+    # When adding another repo (untracked changes) inside an existing repo
+    submodule add https://github.com/LauraAubin/All-things-FED-and-Rails.git Udemy Rails Course/Section 3/test_app
+
+rm -rf <folder/file name> # delete
+
+git checkout -f # undo all changes
+
+# Push an existing repo
+git remote add git@github.com...git
+git push -u origin master
+
+# -------------------------------------------
+# PRODUCTION
+# -------------------------------------------
+
+# Setting up Heroku
+# In gemfile
+group :production do
+  gem 'pg' # postgres
+  gem 'rails_12factor'
+end
